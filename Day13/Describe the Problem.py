f3dbdf4a3d901cdc1709845b592ee8b4eb6689b3
Yuvraj <takcommunity99@gmail.com>
2025-06-25 15:08:29 +0530

    Add files via upload

---
+++ Day13/Describe the Problem.py
@@ -0,0 +1,15 @@
+def my_function():
+    for i in range(1, 21):
+        if i == 20:
+            print("You got it")
+
+
+my_function()
+
+# Describe the Problem - Write your answers as comments:
+# 1. What is the for loop doing?
+    #Ans. In the given program, the for loop is looping i in the range between 1 to 20. 20 is not included. Means 1 to 19. It will execute the block of code which is inside the for loop in the given range.
+# 2. When is the function meant to print "You got it"?
+    #Ans. If i will equals to 20 only then the my_function will print "you got it".
+# 3. What are your assumptions about the value of i?
+    #Ans. The value of i is 1 <= i <20
