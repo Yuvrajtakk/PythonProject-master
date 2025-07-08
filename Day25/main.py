c551a03e7b3ed5caada07f12d8ec2ae8c873f9ef
Yuvraj <takcommunity99@gmail.com>
2025-07-08 21:28:03 +0530

    python

--- Day25/main.py
+++ Day25/main.py
@@ -1,5 +1,9 @@
 with open("weather_data.csv") as file:
     data = file.readlines()
     print(data)
+    line = []
     for name in data:
         stripped_data = name.strip()
+        line.append(stripped_data)
+
+    print(line)
