cc5bd5e6856fff928c2afb0385738912eb53fdb8
Yuvraj <takcommunity99@gmail.com>
2025-07-09 11:24:05 +0530

    python

--- Day25/main.py
+++ Day25/main.py
@@ -1,9 +1,20 @@
-with open("weather_data.csv") as file:
-    data = file.readlines()
-    print(data)
-    line = []
-    for name in data:
-        stripped_data = name.strip()
-        line.append(stripped_data)
+# with open("weather_data") as file:
+#     data = file.readlines()
+#     print(data)
+#     line = []
+#     for name in data:
+#         stripped_data = name.strip()
+#         line.append(stripped_data)
+#
+#     print(line)
 
-    print(line)
+import csv
+
+with open("weather_data") as data_file:
+    data =csv.reader(data_file)
+    tempareture = []
+    for row in data:
+        print(row)
+        if row[1] != "temp":
+            tempareture.append(int(row[1]))
+        print(tempareture)
